f602c99b4fa1a1c35ddf5b8da8809616681f5372
mohamed <mohamedkharashy27@yahoo.com>
2024-01-02 18:02:33 +0000

    1

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,13 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let arr = '', x = 0;
+  for (const el of reportWithIterator) {
+    if (reportWithIterator.length -1 == x) {
+      arr += el
+    }
+    else {
+      arr += el + ' | ';
+    }
+    x++
+  }
+  return arr;
+}
