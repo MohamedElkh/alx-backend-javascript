bd60df13ecbb378984634c17d36db7df8e51c357
mohamed <mohamedkharashy27@yahoo.com>
2024-01-02 17:56:12 +0000

    1

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,5 +1,5 @@
 import getBudgetObject from './7-getBudgetObject.js';
-
+// eslint-disable-next-line no-unused-vars
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
