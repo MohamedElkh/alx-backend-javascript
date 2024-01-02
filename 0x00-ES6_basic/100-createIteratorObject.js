822dda2d312aa228c67a672991d9832aba348a11
mohamed <mohamedkharashy27@yahoo.com>
2024-01-02 17:51:01 +0000

    1

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 export default function createIteratorObject(report) {
   const obj = [];
-  for (const item of Object.values(report.allEmployees)) {
-    obj.push(...item);
+  for (const el of Object.values(report.allEmployees)) {
+    obj.push(...el);
   }
   return obj;
 }
