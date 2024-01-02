4aba43b0e62b6f0a33d6004e3a5d17b34e7bb98f
mohamed <mohamedkharashy27@yahoo.com>
2024-01-02 22:46:32 +0000

    1

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,10 +4,11 @@ import {
 } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise
-    .all([uploadPhoto(), createUser()])
-    .then((res) => {
-      console.log(`${res[0].body} $(res[1].firstName} ${res[1].lastName}`);
-    })
+  const ph = uploadPhoto();
+  const us = createUser();
+
+  return Promise.all([ph, us]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  })
     .catch(() => console.log('Signup system offline'));
 }
