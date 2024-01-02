54127a323fc893b11bbe4d4520d9f776aff71162
mohamed <mohamedkharashy27@yahoo.com>
2024-01-02 23:23:43 +0000

    1

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import {
+  uploadPhoto,
+  createUser,
+} from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    return {
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
+  } catch (err) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
