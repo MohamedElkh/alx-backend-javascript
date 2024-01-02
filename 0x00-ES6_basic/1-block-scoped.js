82a514ea00895176c6e4fd4eace80d6c0e2c3553
mohamed <mohamedkharashy27@yahoo.com>
2024-01-02 15:44:58 +0000

    1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,10 @@
 export default function taskBlock(trueOrFalse) {
+  /* eslint-enable */
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+    /* eslint-enable no-u */
     const task = true;
     const task2 = false;
   }
