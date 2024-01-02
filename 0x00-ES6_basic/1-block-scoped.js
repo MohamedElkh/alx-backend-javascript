ebb2e0d9da9429d7df21fa3bfa39f55704c5a029
mohamed <mohamedkharashy27@yahoo.com>
2024-01-02 16:37:53 +0000

    1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
-/* eslint-enable no-unused-vars */
-/* eslint-enable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
     const task = true;
+    // eslint-disable-next-line no-unused-vars
     const task2 = false;
   }
 
