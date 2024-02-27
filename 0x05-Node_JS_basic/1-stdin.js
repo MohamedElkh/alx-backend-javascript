03ba14a102bb1ee416658466a2ae5b67e7a6f27c
mohamed <mohamedkharashy27@yahoo.com>
2024-02-27 08:12:09 +0000

    1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,13 +3,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 if (process.stdin.isTTY) {
   process.stdin.on('data', (dx) => {
     process.stdout.write(`Your name is: ${dx.toString()}`);
-
     process.exit();
   });
 } else {
   process.stdin.on('data', (dx) => {
     process.stdout.write(`Your name is: ${dx.toString()}`);
-
     process.exit();
   });
 
