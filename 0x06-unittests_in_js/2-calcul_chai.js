31c2104f1037c0aa5ed95b1a865ad0802dc8485f
mohamed <mohamedkharashy27@yahoo.com>
2024-02-28 15:48:08 +0000

    1

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,25 @@
+function calculateNumber(type, a, b) {
+    const anlx = Math.round(a);
+    const bnlx = Math.round(b);
+    let clx = 0;
+
+    switch (type) {
+    case 'SUM':
+	clx = anlx + bnlx;
+	break;
+    case 'SUBTRACT':
+	clx = anlx - bnlx;
+	break;
+    case 'DIVIDE':
+      if (bnlx === 0) {
+            clx = "Error";
+      } else {
+            clx = anlx / bnlx;
+      }
+      break;
+    }
+
+    return clx;
+}
+
+module.exports = calculateNumber;
