428ae39211308824e1d3c7525da15eddbe72a479
mohamed <mohamedkharashy27@yahoo.com>
2024-02-28 16:42:56 +0000

    1

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,15 @@
+const getPaymentTokenFromAPI = require("./6-payment_token");
+const expect = require("chai").expect;
+const {describe, it} = require("mocha");
+
+describe("getPaymentTokenFromAPI", function() {
+    it("Async testing with done callback", function(done) {
+	getPaymentTokenFromAPI(true)
+
+	    .then((data) => {
+		expect(data).to.have.property('data');
+
+		done();
+	    });
+    });
+});
